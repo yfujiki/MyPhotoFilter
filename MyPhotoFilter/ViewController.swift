7210c0b461989038df6e3dc045c57fc62342b836
Yuichi Fujiki <yuichi@chaione.com>
2014-07-15 03:35:57 -0500

    Show acivity view controller from host app

--- MyPhotoFilter/ViewController.swift
+++ MyPhotoFilter/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showActivityViewController() {
+        let image = UIImage(named: "TunnelView")
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.presentViewController(activityViewController, animated: true, completion: nil)
+    }
 }
 
