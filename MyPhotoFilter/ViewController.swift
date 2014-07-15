c7a5cb27c8e50f0e9ae9eb776d014aeb9619d4f1
Yuichi Fujiki <yuichi@chaione.com>
2014-07-15 02:18:07 -0500

    Initial Commit

---
+++ MyPhotoFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPhotoFilter
+//
+//  Created by Yuichi Fujiki on 7/15/14.
+//  Copyright (c) 2014 ChaiONE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
