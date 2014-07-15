2a2aa2457050a00e76480db0da803795db1fef0f
Yuichi Fujiki <yuichi@chaione.com>
2014-07-15 04:21:40 -0500

    Added image filtering

--- MyPhotoFilterAction/ActionViewController.swift
+++ MyPhotoFilterAction/ActionViewController.swift
@@ -29,12 +29,25 @@ extension UIImage {
 }
 
 class ActionViewController: UIViewController {
+    enum ImageFilterOptions: Int {
+        case ColorPosterize = 0
+        case Pixellate
+        case Vignette
+    }
 
     @IBOutlet var imageView: UIImageView?
+    @IBOutlet var slider: UISlider?
+    @IBOutlet var segmentedControl: UISegmentedControl?
+
+    var image:UIImage?
+    var filter:CIFilter?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+
+        self.segmentedControl!.selectedSegmentIndex = 0
+        self.segmentedControlSelected(self.segmentedControl!)
+
         for item: AnyObject in self.extensionContext.inputItems! {
             let inputItem = item as NSExtensionItem
             for provider: AnyObject in inputItem.attachments! {
@@ -54,14 +67,93 @@ class ActionViewController: UIViewController {
 
     func showImageWithSecureCoding(imageData:NSSecureCoding?) {
 
-        let image = UIImage(imageData: imageData)
+        self.image = UIImage(imageData: imageData)
+        var ciImage = CIImage(CGImage: self.image!.CGImage)
+        if let myFilter = self.filter {
+            myFilter.setValue(ciImage, forKey: "inputImage")
+        }
 
-        weak var weakImageView = self.imageView
-        NSOperationQueue.mainQueue().addOperationWithBlock {
-            if let imageView = weakImageView {
-                imageView.image = image
+        self.editImage {
+            editedImage in
+
+            weak var weakImageView = self.imageView
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+                if let imageView = weakImageView {
+                    imageView.image = editedImage
+                }
             }
         }
+    }
+
+    @IBAction func sliderValueChanged(slider:UISlider) {
+        log("Slider value changed to \(slider.value)")
+        self.editImage {
+            editedImage in
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+                self.imageView!.image = editedImage
+            }
+        }
+    }
+
+    @IBAction func segmentedControlSelected(segmentedControl:UISegmentedControl) {
+        log("Segment selected \(segmentedControl.selectedSegmentIndex)")
+
+        switch (segmentedControl.selectedSegmentIndex) {
+        case ImageFilterOptions.ColorPosterize.toRaw():
+            filter = CIFilter(name: "CIColorPosterize")
+        case ImageFilterOptions.Pixellate.toRaw():
+            filter = CIFilter(name: "CIPixellate")
+        case ImageFilterOptions.Vignette.toRaw():
+            filter = CIFilter(name: "CIVignette")
+        default:
+            filter = nil
+        }
+
+        if let myImage = image {
+            if let myFilter = filter {
+                filter!.setValue(CIImage(CGImage: myImage.CGImage), forKey: "inputImage")
+                self.editImage {
+                    editedImage in
+                    NSOperationQueue.mainQueue().addOperationWithBlock {
+                        self.imageView!.image = editedImage
+                    }
+                }
+            }
+        }
+    }
+
+    func editImage(completionBlock:(UIImage?) -> ()) {
+        log("Edit image called")
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {
+            if let myFilter = self.filter {
+                if let myImage = self.image {
+                    switch(self.segmentedControl!.selectedSegmentIndex) {
+                    case ImageFilterOptions.ColorPosterize.toRaw():
+                        myFilter.setValue(self.slider!.value * 10, forKey: "inputLevels")
+                    case ImageFilterOptions.Pixellate.toRaw():
+                        myFilter.setValue(self.slider!.value * 30, forKey: "inputScale")
+
+                        let center = CGPointMake(myImage.size.width / 2, myImage.size.height / 2)
+                        myFilter.setValue(CIVector(CGPoint:center), forKey: "inputCenter")
+                    case ImageFilterOptions.Vignette.toRaw():
+                        myFilter.setValue(self.slider!.value * 100, forKey: "inputRadius")
+                        myFilter.setValue(self.slider!.value * 10, forKey: "inputIntensity")
+                    default:
+                        log("This path should not happen")
+                    }
+
+                    var ciImage = myFilter.outputImage
+                    completionBlock(UIImage(CIImage: ciImage))
+                } else {
+                    log("Image is null")
+                    completionBlock(nil)
+                }
+            } else {
+                log("Filter is null")
+                completionBlock(nil)
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
